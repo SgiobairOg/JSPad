c312a638acbc516b1869d0a4b26dde97fec08a67
Jason Wilson <wilsonsofoxford@gmail.com>
2016-06-17 11:43:16 -0400

    Fixed return type
    Methods were returning 'object' when logged out to the console. Added .valueOf() call to force Javascript to return the String primitive rather than the string object itself.

--- src/string.pad.js
+++ src/string.pad.js
@@ -1,9 +1,9 @@
 String.prototype.padEnd = function ( len, pad='\x20' ) {
   //Pads the string object with the given character to the target length
-  return this.length < len ? ( this + pad ).padEnd( len, pad ) : this 
+  return this.length < len ? ( this + pad ).padEnd( len, pad ) : this.valueOf() 
 }
 
 String.prototype.padStart = function ( len, pad='\x20' ) {
   //Pads the string object with the given character to the target length
-  return this.length < len ? ( pad + this ).padStart( len, pad ) : this 
+  return this.length < len ? ( pad + this ).padStart( len, pad ) : this.valueOf() 
 }
